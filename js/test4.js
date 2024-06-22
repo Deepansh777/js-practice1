d7be6923e063f087cfaf0d899f2d062e4238f0f1
Deepansh777 <dpnsharr@gmail.com>
2024-06-22 09:52:41 -0500

    changing color and cursor style upon hover

--- js/test4.js
+++ js/test4.js
@@ -1,16 +1,35 @@
-// created a parent div
-var parentDiv = document.getElementById('tab1')
+document.addEventListener('DOMContentLoaded', (event) => {
 
-// create a new html tag which unordered list
-const newElement1 = document.createElement('div')
+    // declare let
+    let mouseOver = false;
+    
+    // created a parent div
+    var parentDiv = document.getElementById('tab1')
+    
+    // create a new html tag which unordered list
+    const newElement1 = document.createElement('div')
+    
+    //  run a for loop where i is your incremental
+    for (var i = 0; i<=10; i = i+1){
+        const newElement1a = document.createElement('div');
+        newElement1a.classList.add('box3');
+        newElement1a.id = `div_id-${i}`;
+        newElement1a.innerText = `ID: ${i}`
+        newElement1.append(newElement1a)
+    }
+    
+    // append it to parent div
+    parentDiv.append(newElement1)
+    
+    newElement1.querySelectorAll('.box3').forEach(item => {
+        item.addEventListener( 'mouseover', function(event){
+            mouseOver = true
+            event.target.style.backgroundColor = 'lightblue';
+            event.target.style.cursor = "not-allowed";
+        });
 
-//  run a for loop where i is your incremental
-for (var i = 0; i<=10; i = i+1){
-    const newElement1a = document.createElement('div');
-    newElement1a.classList.add('box3')
-    newElement1a.innerText = `ID: ${i}`
-    newElement1.append(newElement1a)
-}
-
-// append it to parent div
-parentDiv.append(newElement1)
+        item.addEventListener( 'mouseout', function(event){
+            event.target.style.backgroundColor = '';
+        });
+    });
+})
