4bdeef99b2901be8879d97f84645b789bae49557
Deepansh777 <dpnsharr@gmail.com>
2024-06-21 23:54:18 -0500

    initialization

---
+++ js/test1.js
@@ -0,0 +1,6 @@
+function addNumbers(a, b) {
+    return a + b; ;
+}
+x = addNumbers(223, 2);
+
+document.getElementById('test1').innerText = x;
---
+++ js/test2.js
@@ -0,0 +1,3 @@
+const a = "Pritansh Tayade"
+
+document.getElementById('test2').innerText = a;
