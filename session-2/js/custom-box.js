d0832e5ee85a8eba007df42c5cb90bf562f8941e
Deepansh777 <dpnsharr@gmail.com>
2024-06-22 19:11:46 -0500

    custom js function

---
+++ session-2/js/custom-box.js
@@ -0,0 +1,10 @@
+function custom_box(){
+
+    //  get element id of background color
+    const bcg = document.getElementById("bcg");
+    const bt = document.getElementById("bt");
+    const br = document.getElementById("br");
+    const boc = document.getElementById("boc");
+
+
+}
